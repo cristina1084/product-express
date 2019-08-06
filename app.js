b184b337e9a859b204049e08d85d41027af65a11
cristina1084 <cristina.johns1084@gmail.com>
2019-08-06 12:03:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var exp = require('express');
+var bodyparser = require('body-parser');
+const app = exp();
+app.set("view engine","ejs");  //setting view to ejs -- can be set to other templates also
+app.set("views","./src/views");  //specify folder where ejs pages are saved
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+app.get("/newproduct",(req,res)=>{
+    res.render("newprod"); 
+})
+
+app.get("/viewproducts",(req,res)=>{
+    res.render("viewprod");
+})
+
+app.get("/remove",(req,res)=>{
+    res.render("remprod");
+})
+
+app.listen(8080,()=>{
+    console.log("Listening");
+})
