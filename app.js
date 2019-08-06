4e18c9188a13f6cc34023c148d073de4cd00008a
cristina1084 <cristina.johns1084@gmail.com>
2019-08-06 14:00:11 +0530

    added details

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyparser = require('body-parser');
 const app = exp();
 app.set("view engine","ejs");  //setting view to ejs -- can be set to other templates also
 app.set("views","./src/views");  //specify folder where ejs pages are saved
+var prod_array = [{product:"prod1",price:234, qty:90}]
 
 app.use(bodyparser.urlencoded({extended:true}));
 
@@ -15,15 +16,15 @@ app.get("/newproduct",(req,res)=>{
 })
 
 app.get("/viewproducts",(req,res)=>{
-    res.render("viewprod");
+    res.render("viewprod", {prod: prod_array});
 })
 
 app.get("/remove",(req,res)=>{
     res.render("remprod");
 })
 
-app.get("/details",(req,res)=>{
-    res.render("details");
+app.post("/details",(req,res)=>{
+    res.render("details",{product:req.body.product, price:req.body.price, qty:req.body.qty});
 })
 
 app.listen(8080,()=>{
